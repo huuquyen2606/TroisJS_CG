7000f96fa6b9f2b694f135e1c06f48990d00db72
huuquyen2606 <huuquyen2606@gmail.com>
2021-07-08 02:41:11 +0700

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  <!-- <Demo/> -->
+  <Demo/>
 </template>
 
 <script setup>
